97d95660a7ffb1bd1c92ef21b12d0c2f8263ffb9
EarthCompass <mingxuanxiang@gmail.com>
2018-03-28 20:33:07 +0800

    update

--- app/src/main/java/com/example/administrator/a1/MainActivity.kt
+++ app/src/main/java/com/example/administrator/a1/MainActivity.kt
@@ -6,29 +6,32 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import com.example.administrator.a1.R.id.toolbar
+
 
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fab.setOnClickListener{view->startActivity(Intent(this,Camera::class.java))}
+        fab.setOnClickListener { view -> startActivity(Intent(this, Camera::class.java)) }
         if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),101)
+            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 101)
+        }
+        val recyclerView = my_recyclerView.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = MyAdapter(arrayOf("a", "b", "c","wsw","bjh","fbk","fbk","fbk"))
+
         }
         // Example of a call to a native method
 
-       // sample_text.text = stringFromJNI()
-
+        // sample_text.text = stringFromJNI()
     }
-
-
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -46,6 +49,7 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
     fun isExternalStorageWritable(): Boolean {
         return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
     }
@@ -54,6 +58,7 @@ class MainActivity : AppCompatActivity() {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
+
     external fun stringFromJNI(): String
 
     companion object {
